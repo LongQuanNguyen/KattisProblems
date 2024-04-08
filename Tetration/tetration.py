07e76ba6539447f1caee5db7d2c273c851c07332
Long Quan Nguyen <longquanab@gmail.com>
2024-04-08 13:31:22 -0600

    Solved tetration

---
+++ Tetration/tetration.py
@@ -0,0 +1,12 @@
+def main():
+    inp = float(input())
+
+    # a^N = N ->
+    if inp == 1:
+        print(float(1))
+    else:
+        print(inp**(1/inp))
+
+
+if __name__ == '__main__':
+    main()
