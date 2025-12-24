389ac50642d6caec2feca52604a30c38005d7138
OvercookedPotato <longquanab@gmail.com>
2025-12-24 15:04:45 -0600

    Still time limit exceed

--- A+B Problem/a+bproblem.py
+++ A+B Problem/a+bproblem.py
@@ -8,23 +8,30 @@ def main():
             occurrence[num] = 1
         else:
             occurrence[num] += 1
+
     ways = 0
-    for i in range(num_count):
-        for j in range(i+1,num_count):
-            sum = num_list[i] + num_list[j]
-            sum_occurrence = occurrence.get(sum)
-            # print(i, j, num_list[i], num_list[j], sum, sum_occurrence)
-            if sum_occurrence is None:
+
+    key_list = list(occurrence.keys())
+    for i, ai in enumerate(key_list):
+        for j in range(i, len(key_list)):
+            aj = key_list[j]
+            ak =  ai + aj
+            if occurrence.get(ak) is None:
                 continue
-            if sum_occurrence == 1 and (sum == num_list[i] or sum == num_list[j]):
-                continue
-            if sum_occurrence == 2 and sum == num_list[i] and sum == num_list[j]:
-                continue
-            if sum == num_list[i]:
-                sum_occurrence -= 1
-            if sum == num_list[j]:
-                sum_occurrence -= 1
-            ways += 2 * sum_occurrence
+            added_count = 0
+            if ai == aj == ak:
+                added_count = max(0, occurrence[ai] * (occurrence[aj]-1) * (occurrence[ak]-2))
+            elif ai == aj != ak:
+                added_count = occurrence[ai] * (occurrence[ai] - 1) * occurrence[ak]
+            elif ai != aj and aj == ak:
+                added_count = 2 * occurrence[ai] * occurrence[aj] * (occurrence[aj]-1)
+            elif ai != aj and ai == ak:
+                added_count = 2 * occurrence[ai] * occurrence[aj] * (occurrence[ai]-1)
+            elif ai != aj and aj != ak and ai != ak:
+                added_count = 2 * occurrence[ai] * occurrence[aj] * occurrence[ak]
+
+            ways += added_count
+            #print(ai, aj, ak, added_count)
     print(ways)
 
 if __name__ == "__main__":
