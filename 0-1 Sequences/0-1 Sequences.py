7c0cd8cf1c7ff462176d4eccc91e9f2f85ee62a7
OvercookedPotato <longquanab@gmail.com>
2026-08-26 09:12:17 -0600

    0-1 sequence refactor

--- 0-1 Sequences/0-1 Sequences.py
+++ 0-1 Sequences/0-1 Sequences.py
@@ -3,17 +3,16 @@ def main():
     ones = 0
     sequences = 1
     inversions = 0
-    modulus = 10**9 +7
 
     for bit in seq:
         if bit == "1":
-            ones = (ones + sequences) % modulus
+            ones = (ones + sequences)
         if bit == "0":
-            inversions = (inversions + ones) % modulus
+            inversions = (inversions + ones)
         if bit == "?":
-            inversions = (2 * inversions + ones) % modulus
-            ones = (2 * ones + sequences) % modulus
-            sequences = (2 * sequences) % modulus
+            inversions = (2 * inversions + ones)
+            ones = (2 * ones + sequences)
+            sequences = (2 * sequences)
 
     print(inversions)
 
