abba8372b7ab81378366754889b66fe7f9400d53
OvercookedPotato <longquanab@gmail.com>
2026-08-28 11:23:01 -0600

    re-implement freq dict, more efficient but still time exceeded

--- A+B Problem/aplusb.py
+++ A+B Problem/aplusb.py
@@ -12,35 +12,37 @@ def get_freq_dict(int_list):
     return freq_dict
 
 
-def count_triples(int_list, length, freq_dict):
-    total = 0
-
-    for i in range(length):
-        for j in range(length):
-            if i == j:
+def count_triples(freq_dict):
+    ways = 0
+    for value_i, freq_i in freq_dict.items():
+        for value_j, freq_j in freq_dict.items():
+            value_k = value_i + value_j
+            if value_k not in freq_dict:
                 continue
 
-            target_k = int_list[i] + int_list[j]
+            choices_i = freq_i
+            choices_j = freq_j
+            choices_k = freq_dict.get(value_k)
 
-            number_of_k_choices = freq_dict.get(target_k, 0)
-            if number_of_k_choices == 0:
-                continue
+            if value_i == value_j:
+                choices_j -= 1
 
-            if int_list[i] == target_k:
-                number_of_k_choices -= 1
+            if value_k == value_i:
+                choices_k -= 1
 
-            if int_list[j] == target_k:
-                number_of_k_choices -= 1
+            if value_k == value_j:
+                choices_k -= 1
 
-            total += number_of_k_choices
-    return total
+            ways += choices_i * choices_j * choices_k
+
+    return ways
 
 
 def main():
     n = int(input())
     int_list = string_to_int_list(input())
     freq_dict = get_freq_dict(int_list)
-    print(count_triples(int_list, n, freq_dict))
+    print(count_triples(freq_dict))
 
 if __name__ == "__main__":
     main()
