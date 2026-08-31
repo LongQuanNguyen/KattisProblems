9717d2eb8935f723d487305065a1ca8f92ba0762
OvercookedPotato <longquanab@gmail.com>
2026-08-31 12:44:05 -0600

    Solved alex and bard, run into uncaught error, most likely exceeded recursion depth

--- Alex and Barb/alexandbarb.py
+++ Alex and Barb/alexandbarb.py
@@ -1,11 +1,23 @@
-def alex_win(k, n ,m):
+def alex_win(card_left, min_draw , max_draw, cache):
+    if card_left in cache:
+        return cache[card_left]
+
+    if card_left < min_draw:
+        cache[card_left] = False
+        return False
+
+    for draw in range(min_draw, min(max_draw, card_left) + 1):
+        if not alex_win(card_left - draw, min_draw, max_draw, cache):
+            cache[card_left] = True
+            return True
+
+    cache[card_left] = False
     return False
 
-
 def main():
-    k, n, m = map(int, input().split())
-    print("Alex" if alex_win(k, n, m) else "Barb")
-
+    k, m, n = map(int, input().split())
+    cache = {}
+    print("Alex" if alex_win(k, m, n, cache) else "Barb")
 
 if __name__ == "__main__":
     main()
