19a2c8d81f07e7fca1f36c5fa35b2b5c5b9d41f4
OvercookedPotato <longquanab@gmail.com>
2026-09-03 15:30:31 -0600

    refactor to use one matrix, passed all test cases

--- Knapsack/knapsack.py
+++ Knapsack/knapsack.py
@@ -3,7 +3,7 @@ def inspect_matrix(matrix):
         print(row)
 
 
-def bottom_up_dp(optimal_val_table, optimal_take_table, item_weights, item_values, num_items, capacity):
+def bottom_up_dp(optimal_val_table, item_weights, item_values, num_items, capacity):
     for i in range(1, num_items + 1):
         i_value = item_values[i - 1]
         i_weights = item_weights[i - 1]
@@ -18,19 +18,18 @@ def bottom_up_dp(optimal_val_table, optimal_take_table, item_weights, item_value
                 take_value = i_value + optimal_val_table[i - 1][c - i_weights]
 
                 if take_value > skip_value:
-                    optimal_take_table[i][c] = True
                     optimal_val_table[i][c] = take_value
                 else:
                     optimal_val_table[i][c] = skip_value
 
 
-def get_optimal_item_indices(optimal_take_table, num_items, capacity, item_weights):
+def get_optimal_item_indices(optimal_val_table, num_items, capacity, item_weights):
     i = num_items
     c = capacity
     indices = []
 
     while i > 0:
-        if optimal_take_table[i][c]:
+        if optimal_val_table[i][c] != optimal_val_table[i - 1][c]:
             indices.append(i - 1)
             c -= item_weights[i - 1]
 
@@ -52,12 +51,10 @@ def main():
                 item_values.append(value)
 
             optimal_val_table = [[0] * (capacity + 1) for _ in range(num_items + 1)]
-            optimal_take_table = [[False] * (capacity + 1) for _ in range(num_items + 1)]
 
-            bottom_up_dp(optimal_val_table, optimal_take_table, item_weights, item_values, num_items, capacity)
+            bottom_up_dp(optimal_val_table, item_weights, item_values, num_items, capacity)
             #inspect_matrix(optimal_val_table)
-            #inspect_matrix(optimal_take_table)
-            indices = get_optimal_item_indices(optimal_take_table, num_items, capacity, item_weights)
+            indices = get_optimal_item_indices(optimal_val_table, num_items, capacity, item_weights)
 
             print(len(indices))
             print(" ".join(map(str, indices)))
